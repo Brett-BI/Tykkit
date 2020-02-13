df1ff15884fd8ddf8b06566ff3ecb0082cfcdd23
Brett_BI <b.m.burleyinners@gmail.com>
2020-02-12 20:18:21 -0500

    basic routes and start of login process

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, Email
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'bigolesecret'
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+# models
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(70), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+
+# forms
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=5, max=50)])
+    email = StringField('email', validators=[InputRequired(), Email(), Length(max=70)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=60)])
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return 'username: {}, password: {}, and email: {}'.format(form.username.data, form.password.data, form.email.data)
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
