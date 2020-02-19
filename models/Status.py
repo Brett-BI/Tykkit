53b62a88895346ea5502ee3c5407007dc8ff8037
BrettBI <b.m.burleyinners@gmail.com>
2020-02-18 20:12:17 -0500

    Added models for ticket, priority, status and moved DB to the AWS

--- models/Status.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy
-
-class Status(db.Model):
-    __tablename__ = 'status'
-    id = db.Column(db.Integer(), primary_key=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(200), nullable=True)
